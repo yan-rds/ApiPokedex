671aedaea7896ed5b290f90ddd9088485cd2014b
yan-rds <yyan1207@gmail.com>
2021-11-16 20:02:29 -0300

    Criar Enum Tipo

---
+++ src/main/java/com/example/pokedexApi/enuns/Tipo.java
@@ -0,0 +1,6 @@
+package com.example.pokedexApi.enuns;
+
+public enum Tipo {
+    NORMAL, FIRE, FIGHTING, WATER, FLYING, GRASS, POISON, ELECTRIC, GROUND, PSYCHIC, ROCK, ICE,
+    BUG, DRAGON, GHOST, DARK, STEEL, FAIRY, NONE
+}
