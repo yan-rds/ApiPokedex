d885444d566a58d487ee237e160ca0ba917550f6
yan-rds <yyan1207@gmail.com>
2021-11-16 19:52:45 -0300

    Criar model Ability

---
+++ src/main/java/com/example/pokedexApi/abilities/Ability.java
@@ -0,0 +1,16 @@
+package com.example.pokedexApi.abilities;
+
+public class Ability {
+    String nome;
+
+    public Ability() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
