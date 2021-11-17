f5b3fdefdaf262ace69ad66fc2d8ea41f27a197a
yan-rds <yyan1207@gmail.com>
2021-11-17 16:52:43 -0300

    Criar método salvarPokemon na controller

--- src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
+++ src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
@@ -1,11 +1,28 @@
 package com.example.pokedexApi.pokemons;
 
+import com.example.pokedexApi.components.Conversor;
+import com.example.pokedexApi.dtos.EntradaPokemonDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/pokemon")
 public class PokemonController {
 
     @Autowired
-    PokemonService service;
+    private PokemonService service;
+    @Autowired
+    private ModelMapper conversor;
+
+    @PostMapping
+    public void salvarPokemon(@RequestBody @Valid EntradaPokemonDTO entradaPokemonDTO){
+        Pokemon pokemon = conversor.map(entradaPokemonDTO, Pokemon.class);
+        service.salvarPokemon(pokemon);
+    }
 }
