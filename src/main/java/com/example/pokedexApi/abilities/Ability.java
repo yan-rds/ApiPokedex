4db45179b3c8bf3cb71165c2741bcd929ee5daa0
yan-rds <yyan1207@gmail.com>
2021-11-17 16:50:03 -0300

    Adicionar ID int (temporário) à model Ability

--- src/main/java/com/example/pokedexApi/abilities/Ability.java
+++ src/main/java/com/example/pokedexApi/abilities/Ability.java
@@ -14,13 +14,6 @@ public class Ability {
     public Ability() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
@@ -28,5 +21,13 @@ public class Ability {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
