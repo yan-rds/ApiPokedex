8dda90b9ca2dfac6f0d56b5a83547ec2e74617db
yan-rds <yyan1207@gmail.com>
2021-11-18 20:47:18 -0300

    Criar método PUT vincularAoPokemon na controller

--- src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
+++ src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
@@ -3,6 +3,7 @@ package com.example.pokedexApi.pokemons;
 import com.example.pokedexApi.components.Conversor;
 import com.example.pokedexApi.dtos.EntradaPokemonDTO;
 import com.example.pokedexApi.dtos.ExibirListaPokemonDTO;
+import com.example.pokedexApi.dtos.VincularMoveAbilityPokemonDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pokemon")
@@ -33,6 +35,24 @@ public class PokemonController {
         List<ExibirListaPokemonDTO> pokemonsExibidos = new ArrayList<>();
         service.ExibirTodosPokemons().forEach(pokemon -> pokemonsExibidos.add(conversor.map(pokemon, ExibirListaPokemonDTO.class)));
         return pokemonsExibidos;
+    }
+
+    @PutMapping
+    public void vincularAoPokemon (@RequestBody VincularMoveAbilityPokemonDTO vincular){
+        Optional<String> nomeAbility = Optional.ofNullable(vincular.getNomeAbility());
+        Optional<String> nomeMove = Optional.ofNullable(vincular.getNomeMove());
+        if (nomeAbility.isEmpty() & nomeMove.isPresent()){
+            service.vincularMove(nomeMove.get(), vincular.getPokedexPokemon());
+        }
+        else if (nomeAbility.isPresent() & nomeMove.isEmpty()){
+            service.vincularAbility(nomeAbility.get(), vincular.getPokedexPokemon());
+        }
+        else if (nomeAbility.isPresent() & nomeMove.isPresent()){
+            service.vincularMoveEAbility(nomeMove.get(), nomeAbility.get(), vincular.getPokedexPokemon());
+        }
+        else {
+            throw new RuntimeException("Não há dados a atualizar");
+        }
     }
 
 }
