65b6fe13d9fd5dbbb3a7a8072471220d1788367f
yan-rds <yyan1207@gmail.com>
2021-11-18 20:40:46 -0300

    Criar Ability Service

--- src/main/java/com/example/pokedexApi/abilities/AbilityService.java
+++ src/main/java/com/example/pokedexApi/abilities/AbilityService.java
@@ -1,4 +1,12 @@
 package com.example.pokedexApi.abilities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AbilityService {
+
+    @Autowired
+    AbilityRepository abilityRepository;
+
 }
