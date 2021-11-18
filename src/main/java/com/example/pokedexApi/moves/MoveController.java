5003b6ef805f596137b194303b765ca328b2029d
yan-rds <yyan1207@gmail.com>
2021-11-18 14:15:55 -0300

    Criar MoveController

--- src/main/java/com/example/pokedexApi/moves/MoveController.java
+++ src/main/java/com/example/pokedexApi/moves/MoveController.java
@@ -1,2 +1,26 @@
-package com.example.pokedexApi.moves;public class MoveController {
+package com.example.pokedexApi.moves;
+
+import com.example.pokedexApi.dtos.EntradaMoveDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/moves")
+public class MoveController {
+
+    @Autowired
+    MoveService service;
+    @Autowired
+    ModelMapper conversor;
+
+    @PostMapping
+    public void salvarMove (@RequestBody EntradaMoveDTO entradaMoveDTO){
+        Move move = conversor.map(entradaMoveDTO, Move.class);
+        service.salvarMove(move);
+    }
+
 }
