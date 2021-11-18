04d7b67d0e06cbf27c6f3fb069d1f04b488a9f81
yan-rds <yyan1207@gmail.com>
2021-11-18 10:50:45 -0300

    Criar método exibirTodosPokemons na Service

--- src/main/java/com/example/pokedexApi/pokemons/PokemonService.java
+++ src/main/java/com/example/pokedexApi/pokemons/PokemonService.java
@@ -3,6 +3,8 @@ package com.example.pokedexApi.pokemons;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PokemonService {
 
@@ -11,5 +13,9 @@ public class PokemonService {
 
     public void salvarPokemon (Pokemon pokemon){
         repository.save(pokemon);
+    }
+
+    public List<Pokemon> ExibirTodosPokemons() {
+        return (List<Pokemon>) repository.findAll();
     }
 }
