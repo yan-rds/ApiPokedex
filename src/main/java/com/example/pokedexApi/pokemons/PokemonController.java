6621c4f78582fa5d3c8009aaf7992a075d13ce41
yan-rds <yyan1207@gmail.com>
2021-11-18 10:52:27 -0300

    Criar metodo ExibirPokemons na controller

--- src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
+++ src/main/java/com/example/pokedexApi/pokemons/PokemonController.java
@@ -2,14 +2,15 @@ package com.example.pokedexApi.pokemons;
 
 import com.example.pokedexApi.components.Conversor;
 import com.example.pokedexApi.dtos.EntradaPokemonDTO;
+import com.example.pokedexApi.dtos.ExibirListaPokemonDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pokemon")
@@ -21,8 +22,17 @@ public class PokemonController {
     private ModelMapper conversor;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void salvarPokemon(@RequestBody @Valid EntradaPokemonDTO entradaPokemonDTO){
         Pokemon pokemon = conversor.map(entradaPokemonDTO, Pokemon.class);
         service.salvarPokemon(pokemon);
     }
+
+    @GetMapping
+    public List<ExibirListaPokemonDTO> exibirPokemons(){
+        List<ExibirListaPokemonDTO> pokemonsExibidos = new ArrayList<>();
+        service.ExibirTodosPokemons().forEach(pokemon -> pokemonsExibidos.add(conversor.map(pokemon, ExibirListaPokemonDTO.class)));
+        return pokemonsExibidos;
+    }
+
 }
