659ad953c0cb5760134c149f80eccfeef153eeba
yan-rds <yyan1207@gmail.com>
2021-11-18 20:45:18 -0300

    Criar EntradaAbilityDTO

--- src/main/java/com/example/pokedexApi/dtos/EntradaAbilityDTO.java
+++ src/main/java/com/example/pokedexApi/dtos/EntradaAbilityDTO.java
@@ -1,2 +1,16 @@
-package com.example.pokedexApi.dtos;public class EntradaAbilityDTO {
+package com.example.pokedexApi.dtos;
+
+public class EntradaAbilityDTO {
+    private String nome;
+
+    public EntradaAbilityDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
