ab7ec91afa863083a67c8c7f1ce2e494b8c6fd1d
yan-rds <yyan1207@gmail.com>
2021-11-18 20:45:00 -0300

    Criar método salvarAbility na Controller

--- src/main/java/com/example/pokedexApi/abilities/AbilityController.java
+++ src/main/java/com/example/pokedexApi/abilities/AbilityController.java
@@ -1,6 +1,12 @@
 package com.example.pokedexApi.abilities;
 
+import com.example.pokedexApi.dtos.EntradaAbilityDTO;
+import com.example.pokedexApi.dtos.EntradaMoveDTO;
+import com.example.pokedexApi.moves.Move;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +16,12 @@ public class AbilityController {
 
     @Autowired
     AbilityService service;
+    @Autowired
+    ModelMapper conversor;
 
-
+    @PostMapping
+    public void salvarAbility (@RequestBody EntradaAbilityDTO entradaAbilityDTO){
+        Ability ability = conversor.map(entradaAbilityDTO, Ability.class);
+        service.salvarAbility(ability);
+    }
 }
