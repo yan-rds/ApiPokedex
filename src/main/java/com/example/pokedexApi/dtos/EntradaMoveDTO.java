c06bd60c66ea7e3dd3a613f81016f10f5552cbc8
yan-rds <yyan1207@gmail.com>
2021-11-18 14:15:14 -0300

    Criar DTO de entrada para moves

--- src/main/java/com/example/pokedexApi/dtos/EntradaMoveDTO.java
+++ src/main/java/com/example/pokedexApi/dtos/EntradaMoveDTO.java
@@ -1,2 +1,74 @@
-package com.example.pokedexApi.dtos;public class EntradaMoveDTO {
+package com.example.pokedexApi.dtos;
+
+import com.example.pokedexApi.enuns.DamageCategory;
+import com.example.pokedexApi.enuns.Tipo;
+
+public class EntradaMoveDTO {
+
+    private String nome;
+    private int level;
+    private Tipo tipo;
+    private DamageCategory damageCategory;
+    private int power;
+    private String accuracy;
+    private String pp;
+
+    public EntradaMoveDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public DamageCategory getDamageCategory() {
+        return damageCategory;
+    }
+
+    public void setDamageCategory(DamageCategory damageCategory) {
+        this.damageCategory = damageCategory;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(String accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getPp() {
+        return pp;
+    }
+
+    public void setPp(String pp) {
+        this.pp = pp;
+    }
 }
