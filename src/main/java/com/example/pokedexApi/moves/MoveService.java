cdf01b521358873eb4b3f8044fd40e8a971fd32b
yan-rds <yyan1207@gmail.com>
2021-11-18 14:17:00 -0300

    Criar MoveService com método SalvarMove

--- src/main/java/com/example/pokedexApi/moves/MoveService.java
+++ src/main/java/com/example/pokedexApi/moves/MoveService.java
@@ -1,2 +1,17 @@
-package com.example.pokedexApi.moves;public class MoveService {
+package com.example.pokedexApi.moves;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoveService {
+
+    @Autowired
+    MoveRepository repository;
+
+    public void salvarMove (Move move){
+        repository.save(move);
+
+    }
+
 }
