aeee8c8c075dbdcddd43055ca0618266da3f72a1
yan-rds <yyan1207@gmail.com>
2021-11-18 20:46:13 -0300

    Criar DTO para vincular Move e Ability a um Pokemon

--- src/main/java/com/example/pokedexApi/dtos/VincularMoveAbilityPokemonDTO.java
+++ src/main/java/com/example/pokedexApi/dtos/VincularMoveAbilityPokemonDTO.java
@@ -1,2 +1,38 @@
-package com.example.pokedexApi.dtos;public class VincularMoveAbilityPokemon {
+package com.example.pokedexApi.dtos;
+
+import javax.validation.constraints.NotNull;
+
+public class VincularMoveAbilityPokemonDTO {
+
+    @NotNull
+    private int pokedexPokemon;
+    private String nomeMove;
+    private String nomeAbility;
+
+    public VincularMoveAbilityPokemonDTO() {
+    }
+
+    public int getPokedexPokemon() {
+        return pokedexPokemon;
+    }
+
+    public void setPokedexPokemon(int pokedexPokemon) {
+        this.pokedexPokemon = pokedexPokemon;
+    }
+
+    public String getNomeMove() {
+        return nomeMove;
+    }
+
+    public void setNomeMove(String nomeMove) {
+        this.nomeMove = nomeMove;
+    }
+
+    public String getNomeAbility() {
+        return nomeAbility;
+    }
+
+    public void setNomeAbility(String nomeAbility) {
+        this.nomeAbility = nomeAbility;
+    }
 }
