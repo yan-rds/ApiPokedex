9a85816d6af9d550e747b9549d4d4179a9d2387c
yan-rds <yyan1207@gmail.com>
2021-11-18 20:40:26 -0300

    Criar Ability Controller

--- src/main/java/com/example/pokedexApi/abilities/AbilityController.java
+++ src/main/java/com/example/pokedexApi/abilities/AbilityController.java
@@ -1,4 +1,15 @@
 package com.example.pokedexApi.abilities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/abilities")
 public class AbilityController {
+
+    @Autowired
+    AbilityService service;
+
+
 }
