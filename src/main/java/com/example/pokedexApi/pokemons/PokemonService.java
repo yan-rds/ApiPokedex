8ed410e35c58092b1480505f0558b694a90035fc
yan-rds <yyan1207@gmail.com>
2021-11-18 20:47:55 -0300

    Criar na service métodos que possibilitam vínculos específicos

--- src/main/java/com/example/pokedexApi/pokemons/PokemonService.java
+++ src/main/java/com/example/pokedexApi/pokemons/PokemonService.java
@@ -1,21 +1,55 @@
 package com.example.pokedexApi.pokemons;
 
+import com.example.pokedexApi.abilities.Ability;
+import com.example.pokedexApi.abilities.AbilityRepository;
+import com.example.pokedexApi.moves.Move;
+import com.example.pokedexApi.moves.MoveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PokemonService {
 
     @Autowired
-    PokemonRepository repository;
+    PokemonRepository pokemonRepository;
+    @Autowired
+    MoveRepository moveRepository;
+    @Autowired
+    AbilityRepository abilityRepository;
+
 
     public void salvarPokemon (Pokemon pokemon){
-        repository.save(pokemon);
+        pokemonRepository.save(pokemon);
     }
 
     public List<Pokemon> ExibirTodosPokemons() {
-        return (List<Pokemon>) repository.findAll();
+        return (List<Pokemon>) pokemonRepository.findAll();
+    }
+
+    public void vincularAbility (String nomeAbility, int pokedex){
+        Optional<Pokemon> pokemonAtualizar = pokemonRepository.findById(pokedex);
+        Optional<Ability> abilityAtualizar = abilityRepository.findById(nomeAbility);
+        pokemonAtualizar.get().getAbilities().add(abilityAtualizar.get());
+        pokemonRepository.save(pokemonAtualizar.get());
+    }
+
+    public void vincularMove (String nomeMove, int pokedex){
+        Optional<Pokemon> pokemonAtualizar = pokemonRepository.findById(pokedex);
+        Optional<Move> moveAtualizar = moveRepository.findById(nomeMove);
+        pokemonAtualizar.get().getMoves().add(moveAtualizar.get());
+        pokemonRepository.save(pokemonAtualizar.get());
+    }
+
+    public void vincularMoveEAbility (String nomeMove, String nomeAbility, int pokedex){
+        Optional<Pokemon> pokemonAtualizar = pokemonRepository.findById(pokedex);
+        Optional<Move> moveAtualizar = moveRepository.findById(nomeMove);
+        Optional<Ability> abilityAtualizar = abilityRepository.findById(nomeAbility);
+
+           pokemonAtualizar.get().getAbilities().add(abilityAtualizar.get());
+           pokemonAtualizar.get().getMoves().add(moveAtualizar.get());
+           pokemonRepository.save(pokemonAtualizar.get());
     }
 }
